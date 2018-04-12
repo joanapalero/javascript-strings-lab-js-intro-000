0882206e820862b8af394f784339cd5c045c568f
joana.palero24@gmail.com <joana.palero24@gmail.com>
2018-04-12 14:50:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ var greeting = "Hello, everybody!";
 
 var specialGuest = "Neil deGrasse Tyson"
 
-var greeting = "" + specialGuest + "!";
+var greetSpecialGuest = "" + specialGuest + "!";
 
 var topic = "space";
 
 var conversation = `${topic}`;
+`Let's talk about, ${topic}`;
